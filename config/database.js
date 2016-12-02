4d692c2185ad59d137b90ab5f0fc1aa71b186eb7
unknown <dblais01@yahoo.com>
2016-12-02 13:02:00 -0800

    changed mongoose connect to remoteUrl

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl : 'mongodb://blais:d00b5r@ec2-35-163-38-217.us-west-2.compute.amazonaws.com:27017/dummydb',
+    //'node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
